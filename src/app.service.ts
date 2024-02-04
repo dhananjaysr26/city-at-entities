8f179a6d32397b4615719a0278b99abb57e9f2fa
Dhananjay Singh <42992869+dhananjaysr26@users.noreply.github.com>
2024-02-04 16:19:09 +0530

    updated:health check qpi response

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! I am Running!';
+    return '[City At Entities]: I am Running!';
   }
 }
